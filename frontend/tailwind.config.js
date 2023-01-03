16e66a66426a004d38a1db18979e44c2615c20da
Piotr Wendt <wendtpiotr333@gmail.com>
2023-01-03 23:33:05 +0100

    Making landing page more responsive, creating base for contact form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      animation:{
+        slidetop: "slidetop 500ms forwards",
+        slideleft: "slideleft 1000ms forwards",
+        fadein: "fadein 3000ms forwards"
+      },
       backgroundImage: {
         landing: "url('./assets/illustrations/landing.png')",
         beingproductive: "url('./assets/illustrations/beingproductive.png')",
         relax: "url('./assets/illustrations/relax.png')",
+        pgone: "url('./assets/illustrations/one.svg')",
+        pgtwo: "url('./assets/illustrations/two.svg')",
+        pgthree: "url('./assets/illustrations/three.svg')",
+        contact: "url('./assets/illustrations/contact.svg')",
+        
       },
     },
   },
