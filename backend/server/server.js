c94fcbc84b27227cf26332d27d298b6903709dde
Piotr Wendt <wendtpiotr333@gmail.com>
2023-03-17 18:03:11 +0100

    installed mongo, mongoose, created database handler object

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const database = require("../server/databaseHandler.js");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+database.connect();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+// TESTING REGISTER AND LOGIN FORM
 app.post("/api/users", async (req, res) => {
-  console.log(req.body);
+  const data = req.body;
+  switch (data.method) {
+    case "register":
+      return handleRegister(data);
+    case "sign-in":
+      return handleLogin(data);
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+const handleLogin = (data) => {
+  console.log("login data ===", data);
+};
+const handleRegister = (data) => {
+  console.log("register data ===", data);
+};
--- frontend/src/pages/TEST.js
+++ frontend/src/pages/TEST.js
@@ -30,75 +30,152 @@ const TEST = () => {
   });
 
   // SIMULATE LOGIN OR REGISTER
-  const handleData = async () => {
+  const handleLogin = async () => {
     await fetch("../api/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username: username, password: password }),
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        method: "sign-in",
+      }),
+    });
+  };
+  const handleRegister = async () => {
+    await fetch("../api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        method: "register",
+      }),
     });
   };
 
   return (
-    <div className="bg-[#121212] flex justify-center w-full h-full">
-      <div className="w-[80%] bg-[#151515] border-l-2 border-r-2 border-lime-500">
-        <div className="flex justify-center">
-          <h1 className="text-7xl tracking-widest font-bold text-lime-500 mt-2">
-            ADMIN PANEL
-          </h1>
-        </div>
-        {/* SIMULATION OF LOGIN AND REGISTER */}
-        <div className="bg-[#353535] flex justify-center">
-          <form className="flex justify-center items-center flex-col">
-            <div className="mt-5 flex justify-center items-center flex-col">
-              <h2 className="text-white">Pass your username</h2>
-              <input
-                className="text-black"
-                type="text"
-                placeholder="Username..."
-                onChange={(e) => handleUsernameChange(e)}
-                value={username}
-              />
-              {usernameCorectness ? (
-                <h3 className="mt-2 text-transparent text-xs tracking-widest max-sm:text-center">
-                  {usernameInfo}
-                </h3>
-              ) : (
-                <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft max-sm:text-center">
-                  {usernameInfo}
-                </h3>
-              )}
-            </div>
-            <div className="mt-5 flex justify-center items-center flex-col">
-              <h2 className="text-white">Pass your password</h2>
-              <input
-                className="text-black "
-                type="password"
-                placeholder="Psst..."
-                onChange={(e) => handlePasswordChange(e)}
-                value={password}
-              />
-              {passwordCorectness ? (
-                <h3 className="mt-2 text-transparent text-xs tracking-widest">
-                  Password must contain special characters
-                </h3>
-              ) : (
-                <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft ">
-                  Password must contain at least one special character
-                </h3>
-              )}
-            </div>
-            <button
-              onClick={handleData}
-              className="mt-2 mb-2 bg-lime-500 p-2 text-lg rounded-lg"
-            >
+    <>
+      <div className="bg-[#121212] flex justify-center w-full h-full">
+        <div className="w-[80%] bg-[#151515] border-l-2 border-r-2 border-lime-500">
+          <div className="flex justify-center">
+            <h1 className="text-7xl tracking-widest font-bold text-lime-500 mt-2">
               Sign In
-            </button>
-          </form>
+            </h1>
+          </div>
+          {/* SIMULATION OF LOGIN AND REGISTER */}
+          <div className="bg-[#353535] flex justify-center">
+            <form className="flex justify-center items-center flex-col">
+              <div className="mt-5 flex justify-center items-center flex-col">
+                <h2 className="text-white">Pass your username</h2>
+                <input
+                  className="text-black"
+                  type="text"
+                  placeholder="Username..."
+                  onChange={(e) => handleUsernameChange(e)}
+                  value={username}
+                />
+                {usernameCorectness ? (
+                  <h3 className="mt-2 text-transparent text-xs tracking-widest max-sm:text-center">
+                    {usernameInfo}
+                  </h3>
+                ) : (
+                  <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft max-sm:text-center">
+                    {usernameInfo}
+                  </h3>
+                )}
+              </div>
+              <div className="mt-5 flex justify-center items-center flex-col">
+                <h2 className="text-white">Pass your password</h2>
+                <input
+                  className="text-black "
+                  type="password"
+                  placeholder="Psst..."
+                  onChange={(e) => handlePasswordChange(e)}
+                  value={password}
+                />
+                {passwordCorectness ? (
+                  <h3 className="mt-2 text-transparent text-xs tracking-widest">
+                    Password must contain special characters
+                  </h3>
+                ) : (
+                  <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft ">
+                    Password must contain at least one special character
+                  </h3>
+                )}
+              </div>
+              <button
+                onClick={handleLogin}
+                className="mt-2 mb-2 bg-lime-500 p-2 text-lg rounded-lg"
+              >
+                Sign In
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="bg-[#121212] flex justify-center w-full h-full">
+        <div className="w-[80%] bg-[#151515] border-l-2 border-r-2 border-lime-500">
+          <div className="flex justify-center">
+            <h1 className="text-7xl tracking-widest font-bold text-lime-500 mt-2">
+              Register
+            </h1>
+          </div>
+          {/* SIMULATION OF LOGIN AND REGISTER */}
+          <div className="bg-[#353535] flex justify-center">
+            <form className="flex justify-center items-center flex-col">
+              <div className="mt-5 flex justify-center items-center flex-col">
+                <h2 className="text-white">Pass your username</h2>
+                <input
+                  className="text-black"
+                  type="text"
+                  placeholder="Username..."
+                  onChange={(e) => handleUsernameChange(e)}
+                  value={username}
+                />
+                {usernameCorectness ? (
+                  <h3 className="mt-2 text-transparent text-xs tracking-widest max-sm:text-center">
+                    {usernameInfo}
+                  </h3>
+                ) : (
+                  <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft max-sm:text-center">
+                    {usernameInfo}
+                  </h3>
+                )}
+              </div>
+              <div className="mt-5 flex justify-center items-center flex-col">
+                <h2 className="text-white">Pass your password</h2>
+                <input
+                  className="text-black "
+                  type="password"
+                  placeholder="Psst..."
+                  onChange={(e) => handlePasswordChange(e)}
+                  value={password}
+                />
+                {passwordCorectness ? (
+                  <h3 className="mt-2 text-transparent text-xs tracking-widest">
+                    Password must contain special characters
+                  </h3>
+                ) : (
+                  <h3 className="mt-2 text-[#F87474] text-xs tracking-widest animate-slideleft ">
+                    Password must contain at least one special character
+                  </h3>
+                )}
+              </div>
+              <button
+                onClick={handleRegister}
+                className="mt-2 mb-2 bg-lime-500 p-2 text-lg rounded-lg"
+              >
+                Register
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
